821d5c2d97a496a3ec289d2315eaafa802cc9cfc
Eric Horodyski <e.horodyski@ieee.org>
2017-01-18 16:13:28 -0500

    Boiler cleanup.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,19 +4,20 @@ import { StatusBar, Splashscreen } from 'ionic-native';
 
 import { TabsPage } from '../pages/tabs/tabs';
 
-
 @Component({
-  templateUrl: 'app.html'
+	templateUrl: 'app.html'
 })
-export class MyApp {
-  rootPage = TabsPage;
+export class WhatsCloneApp {
+	rootPage = TabsPage;
 
-  constructor(platform: Platform) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      StatusBar.styleDefault();
-      Splashscreen.hide();
-    });
-  }
+	constructor(platform: Platform) {
+		platform.ready().then(() => {
+			// Okay, so the platform is ready and our plugins are available.
+			// Here you can do any higher level native things you might need.
+			if (window.hasOwnProperty('cordova')) {
+				StatusBar.styleDefault();
+				Splashscreen.hide();
+			}
+		});
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,21 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
+import { WhatsCloneApp } from './app.component';
 import { TabsPage } from '../pages/tabs/tabs';
 
 @NgModule({
-  declarations: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
-  ],
-  imports: [
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
-  ],
-  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+	declarations: [
+		WhatsCloneApp,
+		TabsPage
+	],
+	imports: [
+		IonicModule.forRoot(WhatsCloneApp)
+	],
+	bootstrap: [IonicApp],
+	entryComponents: [
+		WhatsCloneApp,
+		TabsPage
+	],
+	providers: [{ provide: ErrorHandler, useClass: IonicErrorHandler }]
 })
-export class AppModule {}
+export class AppModule { }
